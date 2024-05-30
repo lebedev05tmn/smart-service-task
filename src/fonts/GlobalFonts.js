1761b27dc7e04d38ba2157883a749758613db803
lebedev05tmn <lebedev05tmn@gmail.com>
2024-05-30 21:47:57 +0300

    fix font bag

--- src/fonts/GlobalFonts.js
+++ src/fonts/GlobalFonts.js
@@ -11,30 +11,30 @@ import TTFirsNeueRegularWoff from "./TTFirsNeue-Regular.woff";
 import TTFirsNeueRegularTTF from "./TTFirsNeue-Regular.ttf";
 
 export default createGlobalStyle`
-    @font-face {
-        font-family: 'TT Firs Neue';
-        src: url('TTFirsNeue-Bold.eot');
-        src: local('TT Firs Neue Bold'), local('TTFirsNeue-Bold'),
-            url(${TTFirsNeueBoldEot}?#iefix') format('embedded-opentype'),
-            url(${TTFirsNeueBoldWoff2}) format('woff2'),
-            url(${TTFirsNeueBoldWoff}) format('woff'),
-            url(${TTFirsNeueBoldTTF}) format('truetype');
-        font-weight: 700;
-        font-style: normal;
-        font-display: swap;
-    }
+@font-face {
+    font-family: 'TT Firs Neue';
+    src: url('${TTFirsNeueBoldEot}');
+    src: local('TT Firs Neue Bold'), local('TTFirsNeue-Bold'),
+        url('${TTFirsNeueBoldEot}?#iefix') format('embedded-opentype'),
+        url('${TTFirsNeueBoldWoff2}') format('woff2'),
+        url('${TTFirsNeueBoldWoff}') format('woff'),
+        url('${TTFirsNeueBoldTTF}') format('truetype');
+    font-weight: 700;
+    font-style: normal;
+    font-display: swap;
+}
 
-    @font-face {
-        font-family: 'TT Firs Neue';
-        src: url('TTFirsNeue-Regular.eot');
-        src: local('TT Firs Neue Regular'), local('TTFirsNeue-Regular'),
-        url(${TTFirsNeueRegularEot}?#iefix') format('embedded-opentype'),
-        url(${TTFirsNeueRegularWoff2}) format('woff2'),
-        url(${TTFirsNeueRegularWoff}) format('woff'),
-        url(${TTFirsNeueRegularTTF}) format('truetype');
-        font-weight: 400;
-        font-style: normal;
-        font-display: swap;
-    }
+@font-face {
+    font-family: 'TT Firs Neue';
+    src: url('${TTFirsNeueRegularEot}');
+    src: local('TT Firs Neue Regular'), local('TTFirsNeue-Regular'),
+    url('${TTFirsNeueRegularEot}?#iefix') format('embedded-opentype'),
+    url('${TTFirsNeueRegularWoff2}') format('woff2'),
+    url('${TTFirsNeueRegularWoff}') format('woff'),
+    url('${TTFirsNeueRegularTTF}') format('truetype');
+    font-weight: 400;
+    font-style: normal;
+    font-display: swap;
+}
     
 `;
